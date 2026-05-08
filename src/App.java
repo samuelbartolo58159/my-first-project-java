b57331566cb0a1cc1ac20975951765585addad66
samuelbartolo58159 <samuel.bartolo58159@ucaldas.edu.co>
2026-05-08 08:45:22 -0500

    feat create project

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+
+    static String nombre_estudiante = "SAMUEL";
+    static int edad = 17;
+    static double peso = 68.9;
+    static boolean pppp = true;
+    //variables
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println(nombre_estudiante + "" + edad + " " + peso);
+        System.out.println(nombre_estudiante + "/" + edad + "/" + peso);
+        System.out.println(nombre_estudiante + " - " + edad + " - " + peso);
+        System.out.println("me llamo " + nombre_estudiante + " tengo " + edad + " años y peso " + peso);
+        
+        if (pppp) {
+        System.out.println("me pica el pn");
+        }else{
+            System.out.println("pirobo");
+        }
+    }
+}
